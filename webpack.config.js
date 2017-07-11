8d8b92179de7fcd0b1e66e44f54f65de8fb687f5
JohannBillar <jwbillar@gmail.com>
2017-07-11 16:02:43 -0700

    Configure for webpack 2 and renames app to src

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,18 @@ var webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
-  entry: [
-    './app/index.js'
-  ],
+  entry: './src/index.js',
+  devtool: 'cheap-eval-source-map',
   output: {
     path: path.join(__dirname, '/public'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    filename: 'bundle.js'
+    // publicPath: '/'
+  },
+  devServer: {
+    contentBase: './public'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json']
   },
   stats: {
     color: true,
@@ -20,17 +22,15 @@ module.exports = {
     chunks: false
   },
   module: {
-    preLoaders: [
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      }
-    ],
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
         loader: 'babel-loader'
       },
       {
@@ -41,19 +41,43 @@ module.exports = {
         test: /\.scss$/,
         loader: 'style-loader!css-loader!sass-loader'
       }
-    ],
+    ]
   },
-  devServer: {
-    contentBase: './public'
-  },
-  plugins: process.env.NODE_ENV === 'production' ? [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-  ] : [],
+  // module: {
+  //   preLoaders: [
+  //     {
+  //       test: /\.jsx?$/,
+  //       exclude: /node_modules/,
+  //       loader: 'eslint-loader'
+  //     }
+  //   ],
+  //   loaders: [
+  //     {
+  //       test: /\.jsx?$/,
+  //       exclude: /node_modules/,
+  //       loader: 'babel-loader'
+  //     },
+  //     {
+  //       test: /\.(png|jpg|jpeg|gif)$/,
+  //       loader: 'url-loader?limit=20000&name=images/[name].[ext]'
+  //     },
+  //     {
+  //       test: /\.scss$/,
+  //       loader: 'style-loader!css-loader!sass-loader'
+  //     }
+  //   ]
+  // },
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+          new webpack.optimize.DedupePlugin(),
+          new webpack.optimize.OccurrenceOrderPlugin(),
+          new webpack.DefinePlugin({
+            'process.env': {
+              NODE_ENV: JSON.stringify('production')
+            }
+          }),
+          new webpack.optimize.UglifyJsPlugin()
+        ]
+      : []
 };
